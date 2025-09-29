210de0512572161a2a4458ef241b72cb5cbcb3c8
Michal Boguslawski <mboguslawski@griddynamics.com>
2025-09-29 10:45:20 +0200

    All

---
+++ ansible/dynamic_inventory.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+INPUT_JSON="$1"
+
+HOST_1_IP=$(echo $abc | jq -r '.["ip-address-instance-1"].["value"]')
+HOST_2_IP=$(echo $abc | jq -r '.["ip-address-instance-2"].["value"]')
+
+cat <<EOL > ./ansible/inventory.ini
+[web-servers]
+app_server1 $HOST_1_IP ansible_user=ubuntu
+app_server2 $HOST_1_IP ansible_user=ubuntu
+EOL
