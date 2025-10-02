9448fad7567fabf02960f7cd0635d92c61ee7c4f
Michal Boguslawski <mboguslawski@griddynamics.com>
2025-10-02 11:18:46 +0200

    some

--- ansible/dynamic_inventory.sh
+++ ansible/dynamic_inventory.sh
@@ -10,7 +10,7 @@ HOST_2_IP=$( jq -r '.["ip-address-instance-2"].["value"]' $INPUT_JSON )
 
 cat <<EOL > ./ansible/inventory.ini
 app_server1 ansible_host=$HOST_1_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/terraform_gce_key
-app_server2 ansible_host=$HOST_1_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/terraform_gce_key
+app_server2 ansible_host=$HOST_2_IP ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/terraform_gce_key
 
 [web-servers]
 app_server1
